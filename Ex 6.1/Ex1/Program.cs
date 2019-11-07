e0e4951ef7f4c06b4ff226a34a9bcaa49b23b37b
Mário Pinto <46058814+mariopinto18@users.noreply.github.com>
2019-11-07 14:23:49 +0000

    Add files via upload
    Ficha 6.1

---
+++ Ex 6.1/Ex1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace Ex1
+{
+    class Program
+    {
+               
+        static void Main(string[] args)
+        {
+            int[] numeros = new int[10];
+            for (int i=0; i<10; i++)
+            {
+                Console.Write("Número: ");
+                numeros[i] = Convert.ToInt16(Console.ReadLine());
+            }
+            double media = numeros.Average();
+            int cont = 0;
+            foreach (int num in numeros)
+            {
+                if (num > media)
+                {
+                    cont++;
+                }
+            }
+            Console.WriteLine("Existem {0} números acima da média", cont);
+            Console.ReadLine();
+
+        }
+
+    }
+}
