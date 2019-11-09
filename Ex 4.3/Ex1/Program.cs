097292330f97e65534f0c4e9b44674f7923c65c6
Mário Pinto <46058814+mariopinto18@users.noreply.github.com>
2019-11-09 19:49:40 +0000

    Add files via upload
    Ficha 4 - Ex 3

---
+++ Ex 4.3/Ex1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Capicua
+
+            Console.Write("Insira um texto:");
+            string text = Console.ReadLine();
+            string text1 = "";
+            for (int i =text.Length-1; i>=0 ; i--)
+            {
+                text1 += text[i];
+            }
+            if (text==text1)
+            {
+                Console.WriteLine("O texto {0} é capicua", text);
+            }
+            else
+            {
+                Console.WriteLine("O texto {0} não é capicua", text);
+            }
+            Console.ReadLine();
+        }
+    }
+}
