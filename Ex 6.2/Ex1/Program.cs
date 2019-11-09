32659c36f2da659224174968ce62b85b0b853e26
Mário Pinto <46058814+mariopinto18@users.noreply.github.com>
2019-11-09 19:36:22 +0000

    Add files via upload
    Ficha 6 - Ex 2

---
+++ Ex 6.2/Ex1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace Ex1
+{
+    class Program
+    {
+               
+        static void Main(string[] args)
+        {
+            int[] pontuacao = new int[10];
+            int[] positivas = new int[0]; // array com pontuações positivas
+            int j = 0;
+            for (int i=0; i<10; i++)
+            {
+                Console.Write("Pontuação: ");
+                pontuacao[i] = Convert.ToInt16(Console.ReadLine());
+                if (pontuacao[i] >=10)
+                {
+                    j++;
+                    Array.Resize(ref positivas, j); // redimensiona array antes de 
+                    positivas[j-1] = pontuacao[i];       // acrescentar dados
+                  
+                 
+                }
+            }
+
+            for(int i = 0; i<j; i++)
+            {
+                Console.Write(positivas[i] + "\t");
+            }
+         
+            Console.ReadLine();
+
+        }
+
+    }
+}
