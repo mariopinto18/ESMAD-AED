62531c92fdef33b1e4bedcfe3b03b676426ed0a5
Mário Pinto <46058814+mariopinto18@users.noreply.github.com>
2019-11-09 19:45:33 +0000

    Add files via upload
    Ficha 4 - Ex 6

---
+++ Ex 4.6/Ex1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ler um nome e imprime nome normalizado / abreviado
+
+            Console.Write("Insira um nome:");
+            string name = Console.ReadLine();
+            // primeiro nome
+            int pos = name.IndexOf(" ");
+            string name_abr = name.Substring(0, pos);
+            
+            // iniciais dos restantes nomes
+            for (int i =pos; i<name.LastIndexOf(" "); i++)
+            {
+                if (name[i] == ' ')
+                {
+                    name_abr = name_abr + " " + name[i + 1] + ".";
+                }
+            }
+            // ultimo nome
+            pos = name.LastIndexOf(" ");
+            string last_name = name.Substring(pos+1, name.Length- pos-1);
+            name_abr = name_abr + " " + last_name;
+
+            Console.WriteLine("Nome Abreviado: {0}", name_abr);
+            Console.ReadLine();
+        }
+    }
+}
